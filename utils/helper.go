642008c5f9d18bb0a27363e40e71d6780cfb066f
Veli Ulugut <veliulugut1@gmail.com>
2024-08-05 22:39:24 +0300

    getlanguage for locarization

--- utils/helper.go
+++ utils/helper.go
@@ -1,6 +1,11 @@
 package utils
 
-import "regexp"
+import (
+	"regexp"
+
+	"github.com/gofiber/fiber/v2"
+
+)
 
 func EmailRegex(email string) bool {
 	regexpEmail := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,4}$`)
@@ -10,4 +15,14 @@ func EmailRegex(email string) bool {
 func PhoneRegex(phoneNumber string) bool {
 	regexPhone := regexp.MustCompile(`^\+[0-9]{1,3}[\s.-]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}([-\s\.]?[0-9]){4,6}$`)
 	return regexPhone.MatchString(phoneNumber)
+}
+
+func GetLanguage(ctx *fiber.Ctx) string {
+	lang := ctx.Get("Accept-Language")
+
+	if lang == "" {
+		lang = "en"
+	}
+
+	return lang
 }
